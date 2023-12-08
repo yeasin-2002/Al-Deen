fee68b0164831f9fc98192d6c73d077ed93e2279
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-12-08 23:45:16 +0600

    made  eslint  more strict by adding other rules and plugging

---
+++ .eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "prettier",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+    "plugin:@typescript-eslint/strict",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules"],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "react-refresh",
+    "@tanstack/query",
+    "perfectionist",
+    "unicorn",
+    "@typescript-eslint",
+    "prettier",
+  ],
+  rules: {
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/prefer-query-object-syntax": "error",
+    "@tanstack/query/stable-query-client": "error",
+    "@typescript-eslint/no-empty-interface": 0,
+    " '@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-unused-vars": 0,
+
+    "prettier/prettier": 1,
+  },
+  root: true,
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/disable-type-checked",
+        "next/core-web-vitals",
+      ],
+    },
+  ],
+};
+
+// {
+//   "extends": "next/core-web-vitals"
+// }
